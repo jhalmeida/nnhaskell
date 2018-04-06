71f5d3efdf7f05de7b1e118bda5a8f6545b18d79
jhalmeida <justin.h.almeida@gmail.com>
2018-04-05 22:14:26 -0700

    one solved

--- 001.hs
+++ 001.hs
@@ -3,3 +3,8 @@
 -- (Note that the Lisp transcription of this problem is incorrect.)
 
 -- Example in Haskell:
+
+myLast :: [a] -> a
+myLast [] = error "no result for empty"
+myLast [x] = x
+myLast (x:xs) = myLast xs
